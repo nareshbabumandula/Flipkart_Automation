5393bee1dd0cbbf957dca410345023a9f96af228
pathav <30514570+pathav@users.noreply.github.com>
2017-08-14 22:59:28 -0400

    i made a change
    change in desc

--- Flipkart_Automation/src/test/java/com/application/scripts/TC03.java
+++ Flipkart_Automation/src/test/java/com/application/scripts/TC03.java
@@ -3,7 +3,7 @@ package com.application.scripts;
 public class TC03 {
 
 	public static void main(String[] args) {
-		System.out.println("Executing AddToCart method")
+		System.out.println("Executing AddToCart method....")
 	}
 
 }
