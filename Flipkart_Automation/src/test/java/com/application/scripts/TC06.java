1418a5985169c5db09a84707d3378c3867a3861c
Mandula Naresh Babu <naresh2223@gmail.com>
2018-02-27 22:29:36 -0500

    Revert "Modified the print statement in TC06"
    This reverts commit a213feb028c77421491eb9dfa46e865f0b0c6d95.
    
    # Conflicts:
    #	Flipkart_Automation/src/test/java/com/application/scripts/TC06.java

--- Flipkart_Automation/src/test/java/com/application/scripts/TC06.java
+++ Flipkart_Automation/src/test/java/com/application/scripts/TC06.java
@@ -7,8 +7,12 @@ public class TC06 {
 	@Test
 	public void testTC06()
 	{
+<<<<<<< HEAD
 		System.out.println("Executing Search product method");
 		System.out.println("Successfully searched the product");
+=======
+		System.out.println("Executing TC06 method");
+>>>>>>> parent of a213feb... Modified the print statement in TC06
 	}
 
 }
