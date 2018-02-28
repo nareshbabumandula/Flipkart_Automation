db15a22d8e24846230f453fcf85eec57b78fbbfa
Mandula Naresh Babu <naresh2223@gmail.com>
2018-02-27 22:35:41 -0500

    Added print statement to TC06 case
    Testing

---
+++ Flipkart_Automation/src/test/java/com/application/scripts/TC06.java
@@ -0,0 +1,13 @@
+package com.application.scripts;
+
+import org.testng.annotations.Test;
+
+public class TC06 {
+	
+	@Test
+	public void testTC06()
+	{
+		System.out.println("Executing Search product method");
+	}
+
+}
