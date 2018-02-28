39f490e5dbc050b92ff59df6bdde268ff46a72f9
Mandula Naresh Babu <naresh2223@gmail.com>
2018-02-27 22:32:25 -0500

    Test
    Testing

--- Flipkart_Automation/src/test/java/com/application/scripts/TC06.java
+++ Flipkart_Automation/src/test/java/com/application/scripts/TC06.java
@@ -9,8 +9,12 @@ public class TC06 {
 	{
 <<<<<<< HEAD
 <<<<<<< HEAD
+<<<<<<< HEAD
 		System.out.println("Executing Search product method");
 		System.out.println("Successfully searched the product");
+=======
+		System.out.println("Executing TC06 method");
+>>>>>>> parent of a213feb... Modified the print statement in TC06
 =======
 		System.out.println("Executing TC06 method");
 >>>>>>> parent of a213feb... Modified the print statement in TC06
