10df4fcfb4fe8578b38d45911f0aa67adb19eb64
Mandula Naresh Babu <naresh2223@gmail.com>
2018-03-01 21:03:31 -0500

    01-Mar-18: Naresh: Added TC07

---
+++ Flipkart_Automation/src/test/java/com/application/scripts/TC07.java
@@ -0,0 +1,15 @@
+package com.application.scripts;
+
+import org.testng.annotations.Test;
+
+public class TC07 {
+	
+	@Test
+	public void testTC07()
+	{
+		System.out.println("Executing Checkout product method");
+		System.out.println("Successfully checked out the product");
+		
+	}
+
+}
