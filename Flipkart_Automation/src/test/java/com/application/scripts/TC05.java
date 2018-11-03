02f4213e65a9d0f0e0cc5dfb2063e8b6f8ab0315
Mandula Naresh Babu <naresh2223@gmail.com>
2018-11-02 22:48:21 -0400

    02-Nov-18: Naresh: Modified the print statement in TCO5

--- Flipkart_Automation/src/test/java/com/application/scripts/TC05.java
+++ Flipkart_Automation/src/test/java/com/application/scripts/TC05.java
@@ -3,7 +3,7 @@ package com.application.scripts;
 public class TC05 {
 
 	public static void main(String[] args) {
-		System.out.println("Executing TC05 updated");
+		System.out.println("Executing TC05 for product checkout");
 
 	}
 
