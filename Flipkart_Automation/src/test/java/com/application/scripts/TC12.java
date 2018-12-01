418672ecc40f71b1ac6edfddc52f81f8ffb7b739
Mandula Naresh Babu <naresh2223@gmail.com>
2018-11-30 22:05:55 -0500

    30-Nov-18:Naresh: Added new test case TC12

---
+++ Flipkart_Automation/src/test/java/com/application/scripts/TC12.java
@@ -0,0 +1,13 @@
+package com.application.scripts;
+
+import org.testng.annotations.Test;
+
+public class TC12 {
+	
+	@Test
+	public void verifyProductDetails()
+	{
+		System.out.println("Successfully verified the product price details");
+	}
+
+}
