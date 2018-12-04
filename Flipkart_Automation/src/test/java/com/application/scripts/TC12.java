870ca2d06f35833368c69fd4c7b92a96570ad0f6
Mandula Naresh Babu <naresh2223@gmail.com>
2018-12-04 00:08:58 -0500

    04-Dec-18: Mamatha: Modified the print statement in TC12

--- Flipkart_Automation/src/test/java/com/application/scripts/TC12.java
+++ Flipkart_Automation/src/test/java/com/application/scripts/TC12.java
@@ -7,7 +7,7 @@ public class TC12 {
 	@Test
 	public void verifyProductDetails()
 	{
-		System.out.println("Successfully verified the stock availability and product price details");
+		System.out.println("Verified the stock availability and product price details");
 	}
 
 }
