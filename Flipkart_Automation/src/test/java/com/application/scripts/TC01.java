a2b9bcdaadfaca41790cbd3d51e9ee34c0afc1b8
Mandula Naresh Babu <naresh2223@gmail.com>
2019-05-17 00:00:34 -0400

    Updated Comment in TC01
    Naresh: 05-16-19: Updated comment for excel call object creation

--- Flipkart_Automation/src/test/java/com/application/scripts/TC01.java
+++ Flipkart_Automation/src/test/java/com/application/scripts/TC01.java
@@ -7,31 +7,34 @@ import com.utilities.Xls_Reader;
 
 public class TC01 extends CommonPage
 {
-
-	//Create object for excel
+	//Create object for excel sheet
 	Xls_Reader reader = new Xls_Reader("TestData/Data.xlsx");
+	
 	SearchResPage sp = new SearchResPage();
 	
 	@Test
 	public void testTC01() throws Throwable
 	{
-		
 		String txtSearchTerm = reader.getCellData("Sheet1", "SearchTerm", 2);
-		String txtCategory = reader.getCellData("Sheet1", "Category", 2);
-	
+		String txtCategory = reader.getCellData("Sheet1", "SearchTerm", 2);
+		waitForVisibilityOfElement(txtSearch, "Search Textbox"); //Wait for Search textbox
+		
 		//Enter text in Search text box
 		type(txtSearch, txtSearchTerm, "Product Search Textbox");
+		SuccessReport("Enter Data in Search textbox", "Successfully entered data in the search textbox");
 		//Click on Search button
 		click(btnSearch, "Search button");
+		Thread.sleep(3000);
 		//Get the page title
 		String strText = getTitle();
-		if (strText.toLowerCase().contains(txtCategory.toLowerCase())) {
+		Thread.sleep(3000);
+		if (strText.toLowerCase().contains(txtSearchTerm.toLowerCase())) {
 			SuccessReport("Verify search results page title", "Appropriate page title '" +strText+"' is displayed for the search term - " + txtSearchTerm);
 		} else 
 		{
 			failureReport("Verify search results page title", "Appropriate page title '" +strText+"' is not displayed for the search term - " + txtSearchTerm);
 		}
-		
+		Thread.sleep(3000);
 		//Verify Search Results page heading
 		String strSearchPageHeading = getText(sp.ResultsHeading, "Search Results Page Heading");
 		
@@ -45,7 +48,7 @@ public class TC01 extends CommonPage
 		Thread.sleep(4000);
 		//Click on Product
 		click(sp.imgProduct, "Product");
-	
 	}
 
+	
 }
